6fe01e76eb494cfcbacd50d7884d7094987a02d6
kato_m <kato191313@gmail.com>
2025-09-15 19:07:58 +0900

    feat: Add tests for palatalized sounds and long vowels

--- test_rhyme_checker.py
+++ test_rhyme_checker.py
@@ -14,5 +14,31 @@ class TestRhymeChecker(unittest.TestCase):
         checker = RhymeChecker(base_word, target_word)
         self.assertEqual(checker.results, [True])
 
+    def test_rhyme_checker_palatalized_sounds(self):
+        # Test for "じゃ"
+        base_word = "じゃ"
+        target_word = "いあ"
+        checker = RhymeChecker(base_word, target_word)
+        self.assertEqual(checker.results, [True])
+
+        # Test for "ぴゃ"
+        base_word = "ぴゃ"
+        target_word = "いあ"
+        checker = RhymeChecker(base_word, target_word)
+        self.assertEqual(checker.results, [True])
+
+    def test_rhyme_checker_long_vowels(self):
+        # Test for "ー" (long 'a')
+        base_word = "カー"
+        target_word = "ああ"
+        checker = RhymeChecker(base_word, target_word)
+        self.assertEqual(checker.results, [True])
+
+        # Test for "ー" (long 'i')
+        base_word = "キー"
+        target_word = "いい"
+        checker = RhymeChecker(base_word, target_word)
+        self.assertEqual(checker.results, [True])
+
 if __name__ == "__main__":
     unittest.main()
